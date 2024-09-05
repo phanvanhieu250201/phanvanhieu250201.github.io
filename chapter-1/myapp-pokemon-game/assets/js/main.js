5fe961af848e9b823d900d3232af8b847ba50b96
Hieu Phan <phanvanhieu250201@gmail.com>
2024-09-05 17:42:38 +0700

    commit first

---
+++ chapter-1/myapp-pokemon-game/assets/js/main.js
@@ -0,0 +1,384 @@
+let listcardfour = [
+  {
+    name:"spearow",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/21.png",
+  },
+  {
+    name:"wartortle",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/8.png",
+  },
+  {
+    name:"ponyta",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/77.png",
+  },
+  {
+    name:"gastly",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/92.png",
+  },
+  {
+    name:"spearow",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/21.png",
+  },
+  {
+    name:"wartortle",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/8.png",
+  },
+  {
+    name:"ponyta",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/77.png",
+  },
+  {
+    name:"gastly",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/92.png",
+  },
+];
+let listcardsix = [
+  {
+    name:"spearow",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/21.png",
+  },
+  {
+    name:"wartortle",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/8.png",
+  },
+  {
+    name:"ponyta",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/77.png",
+  },
+  {
+    name:"gastly",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/92.png",
+  },
+  {
+    name:"spearow",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/21.png",
+  },
+  {
+    name:"wartortle",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/8.png",
+  },
+  {
+    name:"ponyta",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/77.png",
+  },
+  {
+    name:"gastly",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/92.png",
+  },
+  {
+    name:"bulbasaur",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+  },
+  {
+    name:"bulbasaur",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+  },
+  {
+    name:"charmande",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+  },
+  {
+    name:"charmande",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+  },
+];
+let listcardeight = [
+  {
+    name:"spearow",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/21.png",
+  },
+  {
+    name:"wartortle",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/8.png",
+  },
+  {
+    name:"ponyta",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/77.png",
+  },
+  {
+    name:"gastly",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/92.png",
+  },
+  {
+    name:"spearow",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/21.png",
+  },
+  {
+    name:"wartortle",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/8.png",
+  },
+  {
+    name:"ponyta",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/77.png",
+  },
+  {
+    name:"gastly",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/92.png",
+  },
+  {
+    name:"bulbasaur",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+  },
+  {
+    name:"bulbasaur",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+  },
+  {
+    name:"charmande",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+  },
+  {
+    name:"charmande",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+  },
+  {
+    name:"pidgey",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/16.png",
+  },
+  {
+    name:"pidgey",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/16.png",
+  },
+  {
+    name:"nidorino",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/32.png",
+  },
+  {
+    name:"nidorino",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/32.png",
+  },
+];
+let listcardten = [
+  {
+    name:"spearow",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/21.png",
+  },
+  {
+    name:"wartortle",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/8.png",
+  },
+  {
+    name:"ponyta",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/77.png",
+  },
+  {
+    name:"gastly",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/92.png",
+  },
+  {
+    name:"spearow",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/21.png",
+  },
+  {
+    name:"wartortle",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/8.png",
+  },
+  {
+    name:"ponyta",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/77.png",
+  },
+  {
+    name:"gastly",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/92.png",
+  },
+  {
+    name:"bulbasaur",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+  },
+  {
+    name:"bulbasaur",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+  },
+  {
+    name:"charmande",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+  },
+  {
+    name:"charmande",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+  },
+  {
+    name:"pidgey",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/16.png",
+  },
+  {
+    name:"pidgey",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/16.png",
+  },
+  {
+    name:"nidorino",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/32.png",
+  },
+  {
+    name:"nidorino",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/32.png",
+  },
+  {
+    name:"clefable",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/36.png",
+  },
+  {
+    name:"clefable",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/36.png",
+  },
+  {
+    name:"zubat",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/41.png",
+  },
+  {
+    name:"zubat",
+    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/41.png",
+  },
+];
+Vue.createApp({
+    data() {
+      return {
+        countModeGame: 4,
+        listcard:listcardfour,
+        active: [true,true,true,true,true,true,true,true],
+        nameold:"",
+        name: "",
+        score:0,
+        vitri:"",
+        vitriold:"",
+        canClick: true, // Cho phép lật thẻ hay không
+        chooseCount: 10,
+      }
+    },
+    methods:{
+        SelectModeGame(count){
+            this.countModeGame = count;
+            
+            console.log(this.countModeGame)
+            if(count == 4){
+              this.listcard = listcardfour;
+            }
+            else if(count == 6){
+              this.listcard = listcardsix;
+            }
+            else if(count == 8){
+              this.listcard = listcardeight;
+            }
+            else if(count == 10){
+              this.listcard = listcardten;
+            }
+            this.active = new Array(this.listcard.length).fill(true);
+        },
+        handleResetGame(){
+            console.log(this.countModeGame)
+        },
+        // handleActive(index, name){
+        //   //console.log(name)
+        //   this.active[index] = false;
+        //   this.nameold = this.name;
+        //   this.vitriold = this.vitri;
+        //   this.name = name;
+        //   this.vitri = index;
+
+        //   console.log("this.score",this.score)
+        //   console.log("this.name",this.name)
+        //   console.log("this.nameold",this.nameold)
+        //   console.log("this.vitri",this.vitri)
+        //   console.log("this.vitriold",this.vitriold)
+
+        //   if(this.nameold == this.name){
+        //     this.score++;
+        //     this.name = "";
+        //     this.nameold = "";
+        //     this.vitri = "";
+        //     this.vitriold = "";
+        //   }
+        //   else{
+        //     setTimeout(() => {
+        //       if(this.vitriold >= 0){
+        //         this.active[this.vitriold] = true;
+        //       }
+        //       if(this.vitri != ""){
+        //         this.active[this.vitri] = true;
+        //       }             
+        //     }, 1000);
+        //   }
+        //   // if(this.name == ""){
+        //   //   this.name = name;
+        //   // }
+        //   // else{
+        //   //   if(this.name == name){
+        //   //     this.score++;
+              
+        //   //   }
+        //   //   else{
+        //   //     setTimeout(() => {
+        //   //       this.active[index] = true;
+        //   //     }, 1000);           
+        //   //   }
+        //   //   this.name = "";  
+        //   // }
+        //   // console.log(this.countChoose)
+        //   // console.log(this.score)
+
+        // }
+        handleActive(index, name) {
+          // Kiểm tra nếu không thể click (đang chờ)
+          if (!this.canClick || !this.active[index]) return;
+    
+          this.active[index] = false;
+          this.nameold = this.name;
+          this.vitriold = this.vitri;
+          this.name = name;
+          this.vitri = index;
+    
+          if (this.nameold === this.name && this.vitriold !== this.vitri) {
+            // Nếu 2 thẻ giống nhau
+            this.score++;
+            
+            this.name = "";
+            this.nameold = "";
+            this.vitri = "";
+            this.vitriold = "";
+          } else if (this.nameold !== "") {
+            // Nếu 2 thẻ khác nhau, lật lại sau 1 giây
+            this.canClick = false; // Vô hiệu hóa click trong khi chờ
+            setTimeout(() => {
+              if (this.vitriold >= 0) {
+                this.active[this.vitriold] = true;
+              }
+              if (this.vitri !== "") {
+                this.active[this.vitri] = true;
+              }
+              this.name = "";
+              this.nameold = "";
+              this.vitri = "";
+              this.vitriold = "";
+              this.canClick = true; // Cho phép lật tiếp
+              this.chooseCount--;
+            }, 1000);
+          }
+        }
+    },
+    // watch:{
+    //   name(newValue, oldValue1){
+    //     console.log("newValue",newValue)
+    //     console.log("oldValue",oldValue1)
+    //     this.name = "";
+
+    //   },
+    //   vitri(newValue, oldValue1){
+    //     console.log("newValue",newValue)
+    //     console.log("oldValue",oldValue1)
+    //   },
+    // }
+    computed: {
+      combinedWatcher() {
+        console.log(this.name);
+        console.log(this.vitri);
+
+        // return {
+        //   name: this.name,
+        //   vitri: this.vitri,
+        // };
+        // if(this.name == ""){
+
+        // }
+      }
+    },
+  }).mount('#app')
